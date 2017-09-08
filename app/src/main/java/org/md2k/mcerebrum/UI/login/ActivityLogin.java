864bd2397e67245486bc3870b290f78f028eae97
monowar <smhssain@gmail.com>
2017-09-08 11:35:56 -0500

    initial commit

--- app/src/main/java/org/md2k/mcerebrum/UI/login/ActivityLogin.java
+++ app/src/main/java/org/md2k/mcerebrum/UI/login/ActivityLogin.java
@@ -2,13 +2,35 @@ package org.md2k.mcerebrum.UI.login;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.beardedhen.androidbootstrap.BootstrapButton;
+import com.rengwuxian.materialedittext.MaterialEditText;
+
 import org.md2k.mcerebrum.R;
 
 public class ActivityLogin extends AppCompatActivity {
-
+    BootstrapButton buttonLogin, buttonCancel;
+    MaterialEditText editTextUserName, editTextPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        buttonCancel = (BootstrapButton) findViewById(R.id.button_login_cancel);
+        buttonLogin = (BootstrapButton) findViewById(R.id.button_login);
+
+        buttonCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
